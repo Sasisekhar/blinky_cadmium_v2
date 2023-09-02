69e766a5d4d0ffe1d7592fcede8ad0acd3fc0268
Sx3K <sasisekhar4817@gmail.com>
2023-09-02 19:01:04 -0400

    Run mbed-configure separately

--- rt_mbed/configure-mbed.sh
+++ rt_mbed/configure-mbed.sh
@@ -4,7 +4,7 @@ wget "https://www.python.org/ftp/python/3.8.14/Python-3.8.14.tgz"
 export PYTHON_VERSION=3.8.14
 export PYTHON_MAJOR=3
 tar -xvzf Python-${PYTHON_VERSION}.tgz
-rm Python-${PYTHON_VERSION}.tgz
+sudo rm Python-${PYTHON_VERSION}.tgz
 cd Python-${PYTHON_VERSION}
 ./configure \
     --prefix=/opt/python/${PYTHON_VERSION} \
@@ -16,10 +16,10 @@ make
 sudo make install
 cd /opt/python/${PYTHON_VERSION}/bin
 sudo ln -s python3.8 python
-echo "PATH=/opt/python/3.8.14/bin/:$""PATH" >> ~/.profile
+sudo echo "PATH=/opt/python/3.8.14/bin/:$""PATH" >> ~/.profile
 . ~/.profile
-cd /workspaces/blinky_cadmium_v2/
 sudo rm -rf Pyhton-3.8.14
+cd /workspaces/blinky_cadmium_v2/
 pip3 install mbed-cli
 wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B"
 mv "gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B" gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
