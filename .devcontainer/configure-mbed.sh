161854d88b67a97453d816d8606a117433aeb75f
Sasisekhar <sasisekhar4817@gmail.com>
2023-09-11 15:49:03 +0000

    Stable on ubuntu

--- .devcontainer/configure-mbed.sh
+++ .devcontainer/configure-mbed.sh
@@ -1,11 +1,14 @@
+export USER=`whoami`
+echo ${USER}
+sudo chown -R ${USER} /workspaces/blinky_cadmium_v2/
 sudo apt update -y
-sudo apt-get install libffi-dev -y
 sudo apt remove --auto-remove python3 python3.10 -y
-wget "https://www.python.org/ftp/python/3.8.14/Python-3.8.14.tgz"
+sudo apt-get install libffi-dev -y
 export PYTHON_VERSION=3.8.14
 export PYTHON_MAJOR=3
+wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
 tar -xvzf Python-${PYTHON_VERSION}.tgz
-sudo rm Python-${PYTHON_VERSION}.tgz
+rm Python-${PYTHON_VERSION}.tgz
 cd Python-${PYTHON_VERSION}
 ./configure \
     --prefix=/opt/python/${PYTHON_VERSION} \
@@ -15,18 +18,18 @@ cd Python-${PYTHON_VERSION}
     LDFLAGS=-Wl,-rpath=/opt/python/${PYTHON_VERSION}/lib,--disable-new-dtags
 make
 sudo make install
+cd ..
+rm -rf Python-${PYTHON_VERSION}/
 cd /opt/python/${PYTHON_VERSION}/bin
 sudo ln -s python3.8 python
-sudo echo "PATH=/opt/python/3.8.14/bin/:$""PATH" >> ~/.profile
+echo "PATH=/opt/python/3.8.14/bin/:$""PATH" >> ~/.profile
 . ~/.profile
-sudo rm -rf Pyhton-3.8.14
-cd /workspaces/blinky_cadmium_v2/
-sudo rm -rf Python-3.8.14
+cd /workspaces/blinky_cadmium_v2/ 
 sudo apt install mercurial -y
 pip3 install mbed-cli
 wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B"
 mv "gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B" gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
-tar jxf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
+tar -vjxf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 rm gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 sudo mv ./gcc-arm-none-eabi-10.3-2021.10/ /opt/
 mbed config -G GCC_ARM_PATH /opt/gcc-arm-none-eabi-10.3-2021.10/bin
